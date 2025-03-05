2b6245bfa79cbba24aadd44781ec0631487bb085
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2025-03-05 22:54:01 +0800

    feat(core): 实现 Index 类用于高效编码查找
    - 新增 Index 类，支持插入、查询、删除和模糊查找功能
    - 实现了将字符串与整数编码之间的映射
    - 添加了基于 Levenshtein 距离的模糊查找功能
    - 通过 unordered_map 提高了查找效率

---
+++ include/index.h
@@ -0,0 +1,23 @@
+﻿
+#ifndef INDEX_H
+#define INDEX_H
+
+#include <string>
+#include <unordered_map>
+
+class Index {
+private:
+    std::unordered_map<std::wstring, int> name_to_code;
+
+    static std::wstring str_to_w_str(const std::string& str);
+    static std::string w_str_to_str(const std::wstring& w_str);
+    static int levenshtein(const std::wstring &s1, const std::wstring &s2);
+    static int levenshtein(const std::string &s1, const std::string &s2);
+public:
+    void insert(const std::string &name, int code);
+    int select(const std::string &name);
+    std::vector<std::string> del(int code);
+    std::vector<int> find(const std::string &name, int max_distance = 2) const;
+};
+
+#endif //INDEX_H
---
+++ src/index.cpp
@@ -0,0 +1,120 @@
+﻿
+#include "../include/index.h"
+
+#include <codecvt>
+#include <iostream>
+#include <locale>
+
+
+std::wstring Index::str_to_w_str(const std::string &str) {
+    using convert_typeX = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_typeX, wchar_t> converterX;
+    return converterX.from_bytes(str);
+}
+
+
+std::string Index::w_str_to_str(const std::wstring &w_str) {
+    using convert_typeX = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_typeX, wchar_t> converterX;
+    return converterX.to_bytes(w_str);
+}
+
+
+int Index::levenshtein(const std::wstring &s1, const std::wstring &s2) {
+    const size_t m = s1.size(), n = s2.size();
+    std::vector<std::vector<int>> dp(m+1, std::vector<int>(n+1, 0));
+
+    for(int i=0; i<=m; ++i) {
+        dp[i][0] = i;
+    }
+
+    for(int j=0; j<=n; ++j) {
+        dp[0][j] = j;
+    }
+
+    for(int i=1; i<=m; ++i) {
+        for(int j=1; j<=n; ++j) {
+            dp[i][j] = std::min({
+                dp[i-1][j]+1,
+                dp[i][j-1]+1,
+                dp[i-1][j-1]+(s1[i-1]!=s2[j-1]) * 2
+            });
+        }
+    }
+
+    return dp[m][n];
+}
+
+
+int Index::levenshtein(const std::string &s1, const std::string &s2) {
+    const size_t m = s1.size(), n = s2.size();
+    std::vector<std::vector<int>> dp(m+1, std::vector<int>(n+1, 0));
+
+    for(int i=0; i<=m; ++i) {
+        dp[i][0] = i;
+    }
+
+    for(int j=0; j<=n; ++j) {
+        dp[0][j] = j;
+    }
+
+    for(int i=1; i<=m; ++i) {
+        for(int j=1; j<=n; ++j) {
+            dp[i][j] = std::min({
+                dp[i-1][j]+1,
+                dp[i][j-1]+1,
+                dp[i-1][j-1]+(s1[i-1]!=s2[j-1]) * 2
+            });
+        }
+    }
+
+    return dp[m][n];
+}
+
+
+
+void Index::insert(const std::string &name, const int code) {
+    name_to_code[str_to_w_str(name)] = code;
+}
+
+
+int Index::select(const std::string &name) {
+    const std::wstring w_name = str_to_w_str(name);
+    if (!name_to_code.count(w_name)) {
+        throw std::out_of_range("name not found");
+    }
+
+    return name_to_code[w_name];
+}
+
+
+std::vector<int> Index::find(const std::string &name, const int max_distance) const {
+    if (name.empty()) {
+        return {};
+    }
+
+    std::vector<int> match;
+
+    for (std::pair<std::wstring, int> kv : name_to_code) {
+        if (levenshtein(kv.first, str_to_w_str(name)) <= max_distance) {
+            match.push_back(kv.second);
+        }
+    }
+
+    return match;
+}
+
+
+std::vector<std::string> Index::del(const int code) {
+    std::vector<std::string> result;
+
+    for (std::pair<std::wstring, int> kv: name_to_code) {
+        if (kv.second == code) {
+            result.push_back(w_str_to_str(kv.first));
+            name_to_code.erase(kv.first);
+        }
+    }
+
+    return result;
+}
+
