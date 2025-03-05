2e36ab04bfc7b891c83cf820c1ce9c570f8d130f
Sun0728 <15064807+sun0728@user.noreply.gitee.com>
2025-03-05 17:17:20 +0800

    feat(cache): 实现 LRU缓存功能
    - 新增 LRUCache 类，支持缓存管理功能
    - 实现插入、删除和查询缓存项的方法
    - 采用哈希表和双向链表结合的方式，保证操作高效性
    - 支持根据索引和名称两种方式操作缓存项

---
+++ include/cache.h
@@ -0,0 +1,28 @@
+﻿
+#ifndef CACHE_H
+#define CACHE_H
+
+#include "datatype.h"
+
+#include <unordered_map>
+#include <list>
+
+class LRUCache {
+private:
+    std::unordered_map<int, std::list<Item>::iterator> code_to_item;
+    std::unordered_map<std::string, std::list<Item>::iterator> name_to_item;
+    std::list<Item> cache;
+    int max_cache = 10;
+
+public:
+    explicit LRUCache(int max_cache_number);
+    void insert(const Item& item);
+
+    bool del(int index);
+    bool del(const std::string& name);
+
+    Item select(int index);
+    Item select(const std::string& name);
+};
+
+#endif //CACHE_H
---
+++ src/cache.cpp
@@ -0,0 +1,86 @@
+﻿#include "../include/cache.h"
+
+#include <stdexcept>
+
+LRUCache::LRUCache(const int max_cache_number) {
+    max_cache = max_cache_number;
+}
+
+
+Item LRUCache::select(const int index) {
+    if (!code_to_item.count(index)) {
+        throw std::out_of_range("No such item");
+    }
+
+    const auto iter = code_to_item.at(index);
+    cache.splice(cache.begin(), cache, iter);
+    return *iter;
+}
+
+
+Item LRUCache::select(const std::string &name) {
+    if (!name_to_item.count(name)) {
+        throw std::out_of_range("No such item");
+    }
+
+    const auto iter = name_to_item.at(name);
+    cache.splice(cache.begin(), cache, iter);
+    return *iter;
+}
+
+
+bool LRUCache::del(const int index) {
+    if (!code_to_item.count(index)) {
+        return false;
+    }
+
+    const auto iter = code_to_item.at(index);
+
+    code_to_item.erase(index);
+    name_to_item.erase(iter->name);
+
+    cache.erase(iter);
+
+    return true;
+}
+
+
+bool LRUCache::del(const std::string &name) {
+    if (!name_to_item.count(name)) {
+        return false;
+    }
+
+    const auto iter = name_to_item.at(name);
+
+    code_to_item.erase(iter->code);
+    name_to_item.erase(name);
+
+    cache.erase(iter);
+
+    return true;
+}
+
+
+void LRUCache::insert(const Item &item) {
+    if (code_to_item.count(item.code)) {
+        // 更新已有键的值并移动到头部
+        const auto iter = code_to_item.at(item.code);
+        *iter = item;
+        cache.splice(cache.begin(), cache, iter);
+        return;
+    }
+
+    // 插入新键到链表头部
+    cache.emplace_front(item);
+    code_to_item[item.code] = cache.begin();
+    name_to_item[item.name] = cache.begin();
+
+    // 超过容量则淘汰尾部元素
+    if (cache.size() > max_cache) {
+        const Item value = cache.back();
+
+        code_to_item.erase(value.code);
+        name_to_item.erase(value.name);
+        cache.pop_back();
+    }
+}
